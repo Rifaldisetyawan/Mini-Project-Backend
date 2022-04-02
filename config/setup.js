29e28b56aacb350f0d2f1e6da892c8375b3bbe00
unknown <rifaldisetyawan21@gmail.com>
2022-04-02 16:40:10 +0700

    add uniqe in merchant_info table and add foreign key in product_info table

--- config/setup.js
+++ config/setup.js
@@ -7,7 +7,8 @@ const createMerchant = `
         name VARCHAR(255) NOT NULL,
         address VARCHAR(255) NOT NULL,
         join_date DATE,
-        phone_number VARCHAR(13)
+        phone_number VARCHAR(13),
+        UNIQUE (name)
     )
 `
 const createProduct = `
@@ -15,9 +16,12 @@ const createProduct = `
         id INTEGER(6) PRIMARY KEY NOT NULL,
         name VARCHAR(255) NOT NULL,
         quantity INTEGER(6),
-        Price INTEGER(12)
+        Price INTEGER(12),
+        merchant_id INTEGER(6) NOT NULL,
+        FOREIGN KEY(merchant_id) REFERENCES merchant_info(id)
     )
 `
+
 db.connect(() => {
     db.query(createMerchant, (err) => {
         if (!err) {
